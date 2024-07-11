7120e933b29130e2bfdd4d5d2765f2621fe5290d
jackjduggan <20094010@mail.wit.ie>
2024-07-11 18:25:32 +0100

    Had to rework things to add authorization functionality.. kinda works

--- main.py
+++ main.py
@@ -6,23 +6,37 @@ import os, base64, json
 load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = "http://localhost:8888/callback"  # Make sure to register this URL in your Spotify app settings
 
-def get_token():
-    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+def get_auth_url():
+    scopes = "user-library-read playlist-read-private"
+    auth_url = (
+        f"https://accounts.spotify.com/authorize?response_type=code"
+        f"&client_id={CLIENT_ID}"
+        f"&scope={scopes}"
+        f"&redirect_uri={REDIRECT_URI}"
+    )
+    return auth_url
 
+print("Go to the following URL to authorize the application:")
+print(get_auth_url())
+
+def get_access_token(auth_code):
     url = "https://accounts.spotify.com/api/token"
     headers = {
-        "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded",
     }
-    data = {"grant_type": "client_credentials"}
+    data = {
+        "grant_type": "authorization_code",
+        "code": auth_code,
+        "redirect_uri": REDIRECT_URI,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
 
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+    response = post(url, headers=headers, data=data)
+    response_data = response.json()
+    return response_data["access_token"]
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
@@ -41,7 +55,20 @@ def get_user_playlist_id(token, playlist_name):
 
     return json_result
 
-# --- --- --- --- --- ---
-token = get_token()
+def get_user_playlists(token):
+    url = "https://api.spotify.com/v1/me/playlists"
+    headers = get_auth_header(token)
 
-result = get_user_playlist_id(token, "House")
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    
+    return json_result
+
+
+
+# --- --- --- --- --- ---
+print("Go to the following URL to authorize the application:")
+print(get_auth_url())
+
+auth_code = input("Enter the authorization code you received from Spotify: ")
+token = get_access_token(auth_code)
